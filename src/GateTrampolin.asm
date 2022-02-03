2f455a4949e7752695f22ac4d769a80a1731f781
thefLink <sebfeldm@gmail.com>
2022-02-03 13:05:59 +0100

    Using save registers

--- src/GateTrampolin.asm
+++ src/GateTrampolin.asm
@@ -2,22 +2,23 @@
 
 PrepareSyscall PROC
 
-	xor r11, r11
-	xor r12, r12
-	mov r11, rcx
-	mov r12, rdx
-	ret
+  xor r11, r11
+  xor r10, r10
+  mov r11, rcx
+  mov r10, rdx
+  ret
+
 
 PrepareSyscall ENDP
 
 DoSyscall Proc
 
-	xor rax, rax
-	mov r10, rcx
-	mov eax, r11d
-	jmp r12
+  push r10
+  xor rax, rax
+  mov r10, rcx
+  mov eax, r11d
+  ret
 	
-
 DoSyscall ENDP
 
 end
